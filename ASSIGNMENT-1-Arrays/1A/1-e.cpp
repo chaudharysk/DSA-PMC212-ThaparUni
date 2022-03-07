237157d2246f7ef10c95dcde7f2f98af29ea4478
Shashikant Chaudhary <47478378+chaudharysk@users.noreply.github.com>
2022-03-08 00:12:22 +0530

    Update 1-e.cpp

--- ASSIGNMENT-1-Arrays/1A/1-e.cpp
+++ ASSIGNMENT-1-Arrays/1A/1-e.cpp
@@ -1,4 +1,4 @@
-// Lower Diagonal
+// Symmetric
 #include<iostream>
 using namespace std;
 int main()
@@ -47,64 +47,34 @@ int main()
         }
     }
     // PRINTING 1D Array
-    arrIndx=0;
+    //arrIndx=0;
     cout<<"************** STORED 1D ARRAY **********"<<endl;
     for(int i=0;i<size;i++)
         {
-            cout<<lowerDiag[arrIndx]<<" ";
-            ++arrIndx;
+            cout<<lowerDiag[i]<<" ";
+            //++arrIndx;
         }
     cout<<endl;
 
-    // SAVING 1D ARRAY to TWO D ARRAY
-    
-    int symMat[100][100];
-    arrIndx=0;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if( (i-j)>=0)
-            {
-                symMat[i][j]=lowerDiag[arrIndx];
-                symMat[j][i]=lowerDiag[arrIndx];
-                ++arrIndx;
-            }
-           
-        }
-        cout<<endl;
-    }
-
-    // PRINTING ABOVE 2D ARRAY
-
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-                cout<<symMat[i][j]<<" ";
-        }
-        cout<<endl;
-    }
 
 
 
-
-    // PRINTING 2D Array
+    // PRINTING Symmetric Matrix 1D Array
     cout<<"************** STORED 1D ARRAY in 2D FORM **********"<<endl;
     arrIndx=0;
      for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
         {
-            if( (i-j)>=0)
+            if( j<=i)
             {
-                cout<<lowerDiag[arrIndx]<<" ";
-                ++arrIndx;
+                int k=(i*(i+1)/2)+j;
+                cout<<lowerDiag[k]<<" ";
             }
             else
             {
-                cout<<lowerDiag[arrIndx]<<" ";
-                ++arrIndx;
+                int k2=(j*(j+1)/2)+i;
+                cout<<lowerDiag3[k2]<<" ";
             }
         }
         cout<<endl;
